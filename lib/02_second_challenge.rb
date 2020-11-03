861a72e932dff459c23e43ac35e65f3746d30ad1
Jacob Tobias <jtobias811@gmail.com>
2020-11-03 16:21:26 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,18 +1,18 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
+   :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      },
-    :capulet => {
+   },
+   :capulet => {
       :patriarch => {},
       :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-      }
-    }
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
 
 
 end
